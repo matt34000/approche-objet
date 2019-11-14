29ff662184ebb212b63dc29aedf0afb6e2bd94b7
Matt34000 <matthieu-le-roy@gmx.fr>
2019-11-14 15:43:32 +0100

    commit

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,27 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+	private int numero;
+	private double solde;
+
+	
+	public Compte(int numero, double solde) {
+		this.numero = numero;
+		this.solde = solde;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public double getSolde() {
+		return solde;
+	}
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+	
+}
